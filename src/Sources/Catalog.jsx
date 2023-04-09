cecf96457086a21db216890feedc0530b30b3939
Maxim <maxim.anokhov@gmail.com>
2023-04-09 17:09:03 +0700

    start

--- src/Sources/Catalog.jsx
+++ src/Sources/Catalog.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import SiteBar from "./SiteBar";
+import  kaory from "./Images/Kaory.png"
 import "./Catalog.css";
 export const Catalog=()=>{
     return(
@@ -9,7 +10,10 @@ export const Catalog=()=>{
         <SiteBar/>
         <div className="catalog_menu">
 
-        <div className="blocks_menu soldered_coolers">ПАЯНЫЕ ТЕПЛООБМЕННИКИ</div>
+        <div className="blocks_menu ">
+        <img src={kaory} className="soldered_coolers"/>
+        ПАЯНЫЕ ТЕПЛООБМЕННИКИ
+        </div>
         <div className="blocks_menu collapsible_coolers">РАЗБОРНЫЕ ТЕПЛООБМЕННИКИ</div>
         </div>
         </div>
