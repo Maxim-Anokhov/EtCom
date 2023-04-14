3f90f7c4f767f4cbf2e7f3c520d14ecba41bea6f
Maxim <maxim.anokhov@gmail.com>
2023-04-14 08:44:29 +0700

    Добавил изображения теплообменников

--- src/Sources/Catalog.jsx
+++ src/Sources/Catalog.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import SiteBar from "./SiteBar";
-import  kaory from "./Images/Kaory.png"
+import { Link } from "react-router-dom";
+// import  kaory from "./Images/Kaory.png"
 import "./Catalog.css";
 export const Catalog=()=>{
     return(
@@ -10,11 +11,8 @@ export const Catalog=()=>{
         <SiteBar/>
         <div className="catalog_menu">
 
-        <div className="blocks_menu ">
-        <img src={kaory} className="soldered_coolers"/>
-        ПАЯНЫЕ ТЕПЛООБМЕННИКИ
-        </div>
         <div className="blocks_menu collapsible_coolers">РАЗБОРНЫЕ ТЕПЛООБМЕННИКИ</div>
+        <div className="blocks_menu  soldered_coolers">  ПАЯНЫЕ ТЕПЛООБМЕННИКИ </div>
         </div>
         </div>
     )
---
+++ src/Sources/CollpsibleCoolers.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+ 
+export const CollapsibleCoolers=()=>{
+    return(
+        <div className="CollapsibleCoolers">
+
+        </div>
+    )
+}
